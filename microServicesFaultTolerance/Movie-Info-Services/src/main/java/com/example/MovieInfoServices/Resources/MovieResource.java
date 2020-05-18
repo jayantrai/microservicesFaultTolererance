e8ec000ed79b38f360abf19934cc492002b2efa5
Jayant Rai <jayant.chamling@outlook.com>
2020-05-18 17:17:33 -0500

    created rest controller for movie-info-services with port 8081

---
+++ microServicesFaultTolerance/Movie-Info-Services/src/main/java/com/example/MovieInfoServices/Resources/MovieResource.java
@@ -0,0 +1,21 @@
+package com.example.MovieInfoServices.Resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.MovieInfoServices.Model.Movie;
+
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+	
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId ) {
+		return new Movie(movieId, "Transformers");
+		
+		
+	}
+}
