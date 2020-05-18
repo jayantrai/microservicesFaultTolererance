225eb7c4596b5d9bfe9256ef7815e4e14b097055
Jayant Rai <jayant.chamling@outlook.com>
2020-05-18 16:56:20 -0500

    initialized spring project

---
+++ microServicesFaultTolerance/Movie-Info-Services/src/main/java/com/example/MovieInfoServices/MovieInfoServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieInfoServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServicesApplication.class, args);
+	}
+
+}
---
+++ microServicesFaultTolerance/Ratings-Data-Services/src/test/java/com/example/RatingsDataServices/RatingsDataServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RatingsDataServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
