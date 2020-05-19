8df3dd1ae4482b3ee36ed48172e6532ecd006232
Jayant Rai <jayant.chamling@outlook.com>
2020-05-18 19:24:33 -0500

    created ratings service

--- microServicesFaultTolerance/Ratings-Data-Services/src/main/java/com/example/RatingsDataServices/Resources/RatingsResource.java
+++ microServicesFaultTolerance/Ratings-Data-Services/src/main/java/com/example/RatingsDataServices/Resources/RatingsResource.java
@@ -1,5 +1,8 @@
 package com.example.RatingsDataServices.Resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,16 @@ public class RatingsResource {
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
 		return new Rating(movieId, 4);
+	}
+	
+	
+	@RequestMapping("/users/{userId}")
+	public List<Rating> getUserRating(@PathVariable("userId") String userId) {
+				List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 3),
+				new Rating("5678", 4)
+				);
+		return ratings;
 	}
 	
 }
