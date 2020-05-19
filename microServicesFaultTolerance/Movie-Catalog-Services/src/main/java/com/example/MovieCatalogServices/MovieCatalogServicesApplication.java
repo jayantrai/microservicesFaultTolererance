4bfc418164550194b2ba191a4e0554684df1a595
Jayant Rai <jayant.chamling@outlook.com>
2020-05-18 19:07:27 -0500

    Api call using WebCLient builder

--- microServicesFaultTolerance/Movie-Catalog-Services/src/main/java/com/example/MovieCatalogServices/MovieCatalogServicesApplication.java
+++ microServicesFaultTolerance/Movie-Catalog-Services/src/main/java/com/example/MovieCatalogServices/MovieCatalogServicesApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServicesApplication {
 	
+	
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
+	}
 	
 	@Bean
 	public RestTemplate restTemplate() {
--- microServicesFaultTolerance/Movie-Catalog-Services/src/main/java/com/example/MovieCatalogServices/Resources/MovieCatalogResource.java
+++ microServicesFaultTolerance/Movie-Catalog-Services/src/main/java/com/example/MovieCatalogServices/Resources/MovieCatalogResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.MovieCatalogServices.Model.CatalogItem;
 import com.example.MovieCatalogServices.Model.Movie;
@@ -22,11 +23,19 @@ public class MovieCatalogResource {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
 //		RestTemplate restTemplate = new RestTemplate();
+		
+//		another method of creating mircoservice calls is webClient.Builder()
+		
+		
+		
 		
 		// hardcoding a list of ratings using arrays
 		List<Rating> ratings = Arrays.asList(
@@ -38,6 +47,15 @@ public class MovieCatalogResource {
 		// making a call to external api
 		return ratings.stream().map(rating -> {
 			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			
+			
+//			Movie movie = webClientBuilder.build()
+//			.get() // .post for post
+//			.uri("http://localhost:8082/movies/" + rating.getMovieId())
+//			.retrieve()
+//			.bodyToMono(Movie.class)
+//			.block();
+			
 			return new CatalogItem(movie.getName(), "Making an api call", rating.getRating());
 			
 		})
