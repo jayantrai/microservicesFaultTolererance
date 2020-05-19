523f406bc1a86ca1768561087cf3e098fe446d00
Jayant Rai <jayant.chamling@outlook.com>
2020-05-18 20:51:51 -0500

    enabling eureka client

--- microServicesFaultTolerance/Movie-Info-Services/src/main/java/com/example/MovieInfoServices/MovieInfoServicesApplication.java
+++ microServicesFaultTolerance/Movie-Info-Services/src/main/java/com/example/MovieInfoServices/MovieInfoServicesApplication.java
@@ -2,8 +2,10 @@ package com.example.MovieInfoServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient 
 public class MovieInfoServicesApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,5 @@ public class MovieInfoServicesApplication {
 	}
 
 }
+
+// not mandatory to annotate @EnableEurekaClient
--- microServicesFaultTolerance/Ratings-Data-Services/src/main/java/com/example/RatingsDataServices/RatingsDataServicesApplication.java
+++ microServicesFaultTolerance/Ratings-Data-Services/src/main/java/com/example/RatingsDataServices/RatingsDataServicesApplication.java
@@ -2,8 +2,10 @@ package com.example.RatingsDataServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingsDataServicesApplication {
 
 	public static void main(String[] args) {
