8d95420f4dc41efca3c6d6af8764470d471ae72f
Jayant Rai <jayant.chamling@outlook.com>
2020-05-19 20:04:53 -0500

    splitting a hystrixCommand into two methods

--- microServicesFaultTolerance/Movie-Catalog-Services/src/main/java/com/example/MovieCatalogServices/Resources/MovieCatalogResource.java
+++ microServicesFaultTolerance/Movie-Catalog-Services/src/main/java/com/example/MovieCatalogServices/Resources/MovieCatalogResource.java
@@ -35,34 +35,14 @@ public class MovieCatalogResource {
 	@HystrixCommand(fallbackMethod = "getFallbackCatalog")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
-//		RestTemplate restTemplate = new RestTemplate();
+
+		UserRating userRating = getUserRating(userId);
 		
-//		another method of creating mircoservice calls is webClient.Builder()
-		
-		// hardcoding a list of ratings using arrays
-//		List<Rating> ratings = Arrays.asList(
-//				 new Rating("1234", 4),
-//				 new Rating("5678", 3)
-//				);
-		
-//		IMPORTANT
-		
-		// instead of hardcord list we are going to use a rest template 
-		UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
-		
-		
-		// looping through arrays
-		// making a call to external api which combines ratings and movie info
 		return userRating.getRatings().stream().map(rating -> {
 			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 			
 			
-//			Movie movie = webClientBuilder.build()
-//			.get() // .post for post
-//			.uri("http://localhost:8082/movies/" + rating.getMovieId())
-//			.retrieve()
-//			.bodyToMono(Movie.class)
-//			.block();
+
 			
 			return new CatalogItem(movie.getName(), "Making an api call", rating.getRating());
 			
@@ -70,13 +50,23 @@ public class MovieCatalogResource {
 
 		.collect(Collectors.toList());
 	}
+
+
+	private UserRating getUserRating(String userId) {
+		return restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+	}
 		
-//		return Collections.singletonList(
-//				new CatalogItem("Titanic", "It is a good movie", 4)
-//				);
+
 		public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
 			return Arrays.asList(new CatalogItem("No Movie", "", 0));
 		}
 		
 
 }
+
+//Movie movie = webClientBuilder.build()
+//.get() // .post for post
+//.uri("http://localhost:8082/movies/" + rating.getMovieId())
+//.retrieve()
+//.bodyToMono(Movie.class)
+//.block();
